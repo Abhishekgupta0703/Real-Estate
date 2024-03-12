a603921127ab2594ea54e8247e86ba0a1822d8b9
Abhishekgupta0703 <ak19250000@gmail.com>
2024-03-12 18:50:50 +0530

    init project

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./Contact.css";
+import { MdCall } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi2";
+
+export default function Contact() {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <div className="flexColStart c-left">
+          <span className="orangeText">Our Contacts</span>
+          <span className="primaryText">Easy To Contact</span>
+          <span className="secondaryText">
+            We always ready to help by providing the best service
+          </span>
+          <div className="flexColStart contactModes">
+            {/* first row */}
+            <div className="flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Call</span>
+                    <span className="secondaryText">9369698575</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Call Now</div>
+              </div>
+
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Chat</span>
+                    <span className="secondaryText">9369698575</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Chat Now</div>
+              </div>
+            </div>
+            {/* Second row */}
+            <div className="flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Video Call</span>
+                    <span className="secondaryText">9369698575</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Video Call Now</div>
+              </div>
+
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <HiChatBubbleBottomCenter size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Message</span>
+                    <span className="secondaryText">9369698575</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Message Now</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flexEnd c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" width={100} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './GetStarted.css'
+export default function GetStarted(){
+    return <section className="g-wrapper">
+        <div className="paddings innerWidth g-containner">
+            <div className="flexColCenter inner-container">
+<span className="primaryText">Get Started With Homzy</span>
+<span className="secondaryText">Subscribe and find super attractive price quotes from <br />
+find Your resince soon</span>
+<button className="button">
+    <a href="mailto:abhishekgupta26267@gmail.com">Get Started</a>
+</button>
+<span></span>
+            </div>
+        </div>
+    </section>
+}
